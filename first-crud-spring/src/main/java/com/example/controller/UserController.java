659cf418d1d549610adbe956bc542073e86cea7e
Anik Baul <anikbaul2@gmail.com>
2026-01-29 02:55:33 +0530

    Layered Architecture Demo Application Done

---
+++ first-crud-spring/src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.example.controller;
+
+import com.example.service.UserService;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+    UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    public void createUser(String name){
+        userService.addUser(name);
+    }
+    public void listUsers(){
+        List<String> users = userService.getAllUsers();
+        System.out.println("All Users:- "+users);
+    }
+}
---
+++ first-crud-spring/src/main/java/com/example/service/UserService.java
@@ -0,0 +1,21 @@
+package com.example.service;
+
+import com.example.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private UserRepository userRepository;
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    public void addUser(String name){
+        System.out.println(name+" Added.");
+        userRepository.save(name);
+    }
+    public List<String> getAllUsers(){
+        return userRepository.findAll();
+    }
+}
