59ace7572d30b9fd7fec7be9fc2abcef2891bd21
Anik Baul <anikbaul2@gmail.com>
2026-01-31 14:15:02 +0530

    Path Variables Added

--- first-spring-boot/first-spring-boot/src/main/java/com/demo/first_spring_boot/app/UserController.java
+++ first-spring-boot/first-spring-boot/src/main/java/com/demo/first_spring_boot/app/UserController.java
@@ -15,7 +15,7 @@ public class UserController {
     private Map<Integer,User> userDb = new HashMap<>();
 
     @GetMapping
-    public List<User> getUser(){
+    public List<User> getUsers(){
         return new ArrayList<>(userDb.values());
     }
 
@@ -38,9 +38,25 @@ public class UserController {
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteUser(@PathVariable int id){
-        if (userDb.containsKey(id))
+        if (!userDb.containsKey(id))
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         userDb.remove(id);
         return ResponseEntity.ok("User Deleted");
+    }
+
+//    @GetMapping("/user","/user/{userId}")
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<User> getUser(@PathVariable(value = "userId",required = false) int id){
+        if (!userDb.containsKey(id))
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        return ResponseEntity.ok(userDb.get(id));
+    }
+
+    @GetMapping("/{userId}/orders/{orderId}")
+    public ResponseEntity<User> getUserOrder(@PathVariable("userId") int id,@PathVariable(required = false)  int orderId){
+        if (!userDb.containsKey(id))
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        return ResponseEntity.ok(userDb.get(id));
     }
 }
